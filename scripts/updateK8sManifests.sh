04147c7822090f25c25581c0309d2dc942c8dfa4
KUMARPAVAN YENUGULA <pkumar.yenugula@gmail.com>
2026-03-07 18:41:00 +0000

    Updated updateK8sManifests.sh

--- scripts/updateK8sManifests.sh
+++ scripts/updateK8sManifests.sh
@@ -15,7 +15,7 @@ cd /tmp/temp_repo
 
 # Make changes to the Kubernetes manifest file(s)
 # For example, let's say you want to change the image tag in a deployment.yaml file
-sed -i "s|image:.*|image: vottingapp/$2:$3|g" k8s-specifications/$1-deployment.yaml
+sed -i "s|image:.*|image: $2/$1:$3|g" k8s-specifications/$1-deployment.yaml
 
 # Add the modified files
 git add .
