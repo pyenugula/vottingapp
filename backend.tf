49c25c0d09c0c49ba0ce922861c06bd1ecd0822a
KUMARPAVAN YENUGULA <pkumar.yenugula@gmail.com>
2026-03-08 20:09:01 +0000

    Deleted backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstate15559"
-    storage_account_name = "tfstate1957"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
